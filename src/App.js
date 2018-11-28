5b27056d1d3cde02d6ca850667c6bf60b8f0e4af
ankitsbhamra <ankitsbhamra@gmail.com>
2018-11-28 21:33:42 +0530

    Created login page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username:'',
+      password:''
+    };
+    this.handleUsernameChnage.bind(this);
+    this.handlePasswordChange.bind(this);
+  }
+
+  handleUsernameChnage(e){
+    this.setState({username:e.target.value});
+  }
+
+  handlePasswordChange(e){
+    this.setState({password:e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <input type = "text" className="inputBox" placeholder="Username" maxlength="10" onchange={this.handleUsernameChange}/>
+
+          <input type="password" className="inputBox" placeholder="Password" onchange={this.handlePasswordChange}/>
+          <div style={{display:'flex'}}>
+          <input type="button" className="inputBox" value="Log In" style={{margin:'2%'}}/> 
+          <input type="button" className="inputBox" value="Sign Up" style={{margin:'2%'}}/> 
+          </div>
         </header>
       </div>
     );
